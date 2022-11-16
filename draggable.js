e8fd1fa814528055c7987a775dec8466795048a7
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-11-16 15:33:21 +0000

    Test

--- draggable.js
+++ draggable.js
@@ -4,7 +4,7 @@ console.log('Hello')
 const callButton = document.getElementById('callButton')
 
 const onClick = async () => {
-    const audio = new Audio("./audio/heyheyhey (mp3cut.net).mp3")
+    const audio = new Audio("./public/heyheyhey (mp3cut.net).mp3")
 
     setTimeout(() => {
         audio.play()
