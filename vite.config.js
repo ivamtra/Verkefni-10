2e84cf177a994bfc3f8527bc2120eae4ee6d9210
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-11-16 16:08:28 +0000

    TEST

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
     build: {
-    assetsInclude: ['*.mp3'],
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 "face-api.min.js": resolve(__dirname, 'index.html'),
-                draggable: resolve(__dirname, 'uploader.html')
+                draggable: resolve(__dirname, 'uploader.html'),
+                sound: 'public/heyheyhey.mp3',
             }
         }
     }
