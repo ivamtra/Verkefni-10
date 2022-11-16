783f51af3c95008e5f9a3999068c45b3a7debf97
ivamtra <97352752+ivamtra@users.noreply.github.com>
2022-11-16 14:23:42 +0000

    Test

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                "face-api.min.js": resolve(__dirname, 'index.html'),
+                draggable: resolve(__dirname, 'uploader.html')
+            }
+        }
+    }
+})
